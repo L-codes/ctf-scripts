fbcaee9f277763ce3b2ebb5a1f9cb979abc19c0e
L <l-codes@qq.com>
2018-12-29 13:31:33 +0800

    Update awd/scanner/synproxy-scan.rb

--- awd/scanner/synproxy-scan.rb
+++ awd/scanner/synproxy-scan.rb
@@ -5,9 +5,7 @@
 #
 __author__ = 'L'
 
-require 'tempfile'
-require 'childprocess'
-require 'shellwords'
+require 'l-tools'
 require 'socket'
 require 'optparse'
 
@@ -17,45 +15,28 @@ banner = %q[
 \___ \\\ V /|  \| | | |_) | '__/ _ \ \/ / | | | \___ \ / __/ _` | '_ \
  ___) || | | |\  | |  __/| | | (_) >  <| |_| |  ___) | (_| (_| | | | |
 |____/ |_| |_| \_| |_|   |_|  \___/_/\_\\\__, | |____/ \___\__,_|_| |_|
-      author: L  version: 2.0.0         |___/
+      author: L  version: 2.1.0         |___/
 
 
 ]
 
-def create_tshark_cmd(args)
-  interface = args[:interface] ? "-i #{args[:interface]}" : ''
+def create_tshark(args)
   ifaces = Socket.ip_address_list.select(&:ipv4?).map(&:ip_address)
   src_hosts = ifaces.map{|ip| " dst host #{ip} "}.join '||'
-  bpf = 'tcp'
-  bpf += " && (#{src_hosts})" if src_hosts
-  cmd = "tshark #{interface} -f \"#{bpf}\" " +
-        "-Y \"tcp.flags.ack==1 && tcp.window_size_value!=0 \" " +
-        #"-Y \"tcp.flags.ack==1 && tcp.window_size_value!=0 && tcp.seq==1\" " +
-        "-T fields -e ip.src_host -e tcp.srcport"
-  puts "[Debug] Exec CMD: #{cmd}" if args[:debug]
-  cmd.shellsplit
+  bpf = src_hosts ? "tcp && (#{src_hosts})" : "tcp"
+  LTools::Tshark.capture(
+    interface: args[:interface],
+    bpf: bpf,
+    filter: 'tcp.flags.ack==1 && tcp.window_size_value!=0',
+    fields: 'ip.src_host tcp.srcport'
+  )
 end
 
-def create_nmap_cmd(args)
-  ports = args[:ports] ? "-p #{args[:ports]}" : ''
-  cmd = "nmap -T5 -sT -Pn -n #{ports}".shellsplit + ARGV
-  puts "[Debug] Exec CMD: #{cmd.shelljoin}" if args[:debug]
-  cmd
-end
-
-def get_result(io)
-  io.rewind
-  result = Hash.new {|hash,key| hash[key] = []}
-  data = io.read
+def print_result(records)
+  ip_port_map = records.group_by(&:first).transform_values{|x| x.map(&:last) }
   ips = `nmap -Pn -sn -n -sL #{ARGV.shelljoin}`.b
-  data.scan(/([.\d]+)\t(\d+)/) do |ip, port|
-    result[ip] << port
-  end
-  result.select{|ip| ips.match? /\b#{Regexp.quote(ip)}\b/ }
-end
-
-def print_result(io)
-  get_result(io).each do |ip, ports|
+  ip_port_map.each do |ip, ports|
+    next unless ips.match? /\b#{Regexp.quote(ip)}\b/
     puts "Scan report for #{ip}"
     puts "PORT   STATE   SERVICE"
     ports.map(&:to_i).sort.uniq.each do |port|
@@ -67,39 +48,32 @@ def print_result(io)
 end
 
 def start(args)
-  tshark_cmd = create_tshark_cmd(args)
-  nmap_cmd = create_nmap_cmd(args)
-
-  tshark = ChildProcess.build(*tshark_cmd)
-  tshark_io = Tempfile.new
-  tshark.io.stdout = tshark_io
+  tshark = create_tshark(args)
   tshark.start
   sleep 1
 
-  nmap = ChildProcess.build(*nmap_cmd)
-  if args[:nmap]
-    nmap.io.inherit!
-  else
-    nmap_io = Tempfile.new
-    nmap.io.stdout = nmap_io
-  end
+  nmap = LTools::Nmap.portscan(
+    ARGV,
+    ports: args[:ports],
+    mode: 'T'
+  )
+  nmap.io_inherit! if args[:nmap]
   nmap.start
   nmap.wait
+
   if args[:nmap]
     sep = '-'*25
     puts "\n\n#{sep + " SYN Proxy Scan Result " + sep}\n\n\n"
   else
-    nmap_io.seek(-100, IO::SEEK_CUR)
-    last = nmap_io.read[/^.*?\Z/]
+    last = nmap.stdout.read[/^.*?\Z/]
   end
 
   sleep 2
   tshark.stop
   
-  print_result(tshark_io)
-  puts last.gsub('Nmap', 'SYN Proxy Scan') if last && !args[:nmap]
+  print_result(tshark.records)
+  puts last.gsub('Nmap', 'SYN Proxy Scan') if last
 end
-
 
 if __FILE__ == $PROGRAM_NAME
   args = {
@@ -120,7 +94,10 @@ if __FILE__ == $PROGRAM_NAME
     opts.separator "  ./synproxy-scan.rb -i eth0 -p 1-20,30-100,3389 192.168.1.1/24 github.com"
   end
   optparser.parse! into: args
-  puts "[Debug] Args: #{args}" if args[:debug]
+  if args[:debug]
+    puts "[Debug] Args: #{args}"
+    LTools.verbose = true
+  end
 
   if ARGV.empty? 
     puts "[!] Please specify target"
