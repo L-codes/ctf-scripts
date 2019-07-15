be17db2c3c5a854abdaa4530a198aba434eb6c3b
L <l-codes@qq.com>
2019-07-15 13:33:20 +0800

    update png_size_recover.rb

--- misc/image/png_size_recover.rb
+++ misc/image/png_size_recover.rb
@@ -8,6 +8,7 @@ require 'crc'
 
 abort "Usage: ./png_size_recover.rb <png_file>" if ARGV.empty?
 
+max = 3000
 @png_file = ARGV[0]
 
 data = File.binread(@png_file)
@@ -18,27 +19,43 @@ org_high  = data[20,4]
 ihdr_tail = data[24,5]
 crc       = data[29,4]
 
+ihdr = ihdr_head + org_width + org_high + ihdr_tail
+if CRC.crc32.digest(ihdr) == crc
+  abort 'No problem with the picture'
+end
 
-def recover_file(i, data, name)
-  if name == :width
-    data[16,4] = [i].pack('i>')
-  else
-    data[20,4] = [i].pack('i>')
+
+def recover_file(data, values)
+  values.each do |name, i|
+    if name == :width
+      data[16,4] = [i].pack('i>')
+    else
+      data[20,4] = [i].pack('i>')
+    end
+    puts "[+] recover #{name}: #{i}"
   end
+
   file = "recover_#{@png_file}"
   File.binwrite(file, data)
-  puts "[+] recover #{name}: #{i}"
   puts "[+] save to: #{file}"
   exit()
 end
 
 
-65536.times.find do |i|
+max.times do |i|
   ihdr = ihdr_head + [i].pack('i>') + org_high + ihdr_tail
-  recover_file(i, data, :width) if CRC.crc32.digest(ihdr) == crc
+  recover_file(data, width: i) if CRC.crc32.digest(ihdr) == crc
 
   ihdr = ihdr_head + org_width + [i].pack('i>') + ihdr_tail
-  recover_file(i, data, :high) if CRC.crc32.digest(ihdr) == crc
+  recover_file(data, high: i) if CRC.crc32.digest(ihdr) == crc
+end
+
+(1..max).each do |wi|
+  width = [wi].pack('i>')
+  (1..max).each do |hi|
+    ihdr = ihdr_head + width + [hi].pack('i>') + ihdr_tail
+    recover_file(data, width: wi, high: hi) if CRC.crc32.digest(ihdr) == crc
+  end
 end
 
 puts "[!] recover fail"
