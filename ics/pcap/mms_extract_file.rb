0c13a6380967b3927dfb801c39355a452a3bab79
L <l-codes@qq.com>
2019-07-28 23:28:18 +0800

    update mms_extract_file.rb

--- ics/pcap/mms_extract_file.rb
+++ ics/pcap/mms_extract_file.rb
@@ -16,8 +16,11 @@ unless pcap_file
 end
 
 if extract_file
+  # 查找文件对应的 invokeID
   invokeids = `tshark -r #{pcap_file} -Y 'mms.FileName_item == "#{extract_file}"' -Tfields -e mms.invokeID`.split.join(' ')
+  # 对应 invokeID 的 frsmID
   frsmids = `tshark -r #{pcap_file} -Y 'mms.invokeID in {#{invokeids}} && mms.confirmedServiceResponse && mms.frsmID' -Tfields -e mms.frsmID`.split.join(' ')
+  # 通过 frsmID 查找到对应内容请求包
   invokeids = `tshark -r #{pcap_file} -Y 'mms.fileRead in {#{frsmids}} && mms.confirmedServiceRequest' -Tfields -e mms.invokeID`.split
 
   invokeids.each do |invokeid|
