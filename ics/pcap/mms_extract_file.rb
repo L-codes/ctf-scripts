fcd35361fa486bdf8fe4a39c2550c0af97e10d1b
L <l-codes@qq.com>
2019-07-29 21:45:56 +0800

    fix

--- ics/pcap/mms_extract_file.rb
+++ ics/pcap/mms_extract_file.rb
@@ -17,16 +17,16 @@ end
 
 if extract_file
   # 查找文件对应的 invokeID
-  invokeids = `tshark -r #{pcap_file} -Y 'mms.FileName_item == "#{extract_file}"' -Tfields -e mms.invokeID`.split.join(' ')
+  invokeids = `tshark -r '#{pcap_file}' -Y 'mms.FileName_item == "#{extract_file}"' -Tfields -e mms.invokeID`.split.join(' ')
   # 对应 invokeID 的 frsmID
-  frsmids = `tshark -r #{pcap_file} -Y 'mms.invokeID in {#{invokeids}} && mms.confirmedServiceResponse && mms.frsmID' -Tfields -e mms.frsmID`.split.join(' ')
+  frsmids = `tshark -r '#{pcap_file}' -Y 'mms.invokeID in {#{invokeids}} && mms.confirmedServiceResponse && mms.frsmID' -Tfields -e mms.frsmID`.split.join(' ')
   # 通过 frsmID 查找到对应内容请求包
-  invokeids = `tshark -r #{pcap_file} -Y 'mms.fileRead in {#{frsmids}} && mms.confirmedServiceRequest' -Tfields -e mms.invokeID`.split
+  invokeids = `tshark -r '#{pcap_file}' -Y 'mms.fileRead in {#{frsmids}} && mms.confirmedServiceRequest' -Tfields -e mms.invokeID`.split
 
   invokeids.each do |invokeid|
-     hex_data = `tshark -r #{pcap_file} -Y 'mms.invokeID == #{invokeid} && mms.confirmedServiceResponse' -Tfields -e mms.fileData`
+     hex_data = `tshark -r '#{pcap_file}' -Y 'mms.invokeID == #{invokeid} && mms.confirmedServiceResponse' -Tfields -e mms.fileData`
      p [hex_data].pack('H*')
   end
 else
-  system "tshark -r #{pcap_file} -Y mms.FileName_item -Tfields -e mms.FileName_item | sort -u"
+  system "tshark -r '#{pcap_file}' -Y mms.FileName_item -Tfields -e mms.FileName_item | sort -u"
 end
