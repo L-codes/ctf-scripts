926e00f6cdd62c3f9a902421efbca2c77ddd1f98
L <l-codes@qq.com>
2019-07-30 10:38:16 +0800

    update mms_extract_file.rb file list

--- ics/pcap/mms_extract_file.rb
+++ ics/pcap/mms_extract_file.rb
@@ -3,6 +3,9 @@
 # Author L
 #
 
+require 'oj'
+require 'filesize'
+
 pcap_file = ARGV[0]
 extract_file = ARGV[1]
 
@@ -35,5 +38,32 @@ if extract_file
     end
   end
 else
-  system "tshark -r '#{pcap_file}' -Y mms.FileName_item -Tfields -e mms.FileName_item | sort -u"
+  file_directory_json = `tshark -r #{pcap_file} -Y 'mms.confirmedServiceResponse == 77' -Tjson -e mms.FileName_item -e mms.sizeOfFile -e mms.lastModified`
+  file_directory = Oj.load(file_directory_json).map{|data| a, *b = data["_source"]["layers"].values; a.zip(*b) }.flatten(1).uniq
+
+  mms_fileopen_request =
+    `tshark -r #{pcap_file} -Y 'mms.confirmedServiceRequest == 72' -Tfields -e mms.invokeID -e mms.FileName_item`.
+    each_line(chomp: true).
+    map(&:split)
+
+  mms_fileopen_response =
+    `tshark -r #{pcap_file} -Y 'mms.confirmedServiceResponse == 72' -Tfields -e mms.invokeID -e mms.sizeOfFile -e mms.lastModified`.
+    each_line(chomp: true).
+    map{|x| id, size, time = x.split("\t", 3); [id, size, time]}
+
+  mms_fileopen = []
+  mms_fileopen_request.each do |id, name|
+    _, size, time = mms_fileopen_response.find{|x| x.first == id}
+    mms_fileopen << [name, size, time] if size && time
+  end
+  mms_fileopen.uniq!
+
+
+  read_files = mms_fileopen_request.map(&:last).uniq
+  files = (file_directory | mms_fileopen).uniq{|x| x[0..1]}
+  puts "%-25s  %11s\t  %-4s   %s" % ['Time', 'Size', 'OP', 'FileName']
+  files.each do |name, size, time|
+    read = read_files.include?(name) ? 'READ' : '    '
+    puts "%-25s  %11s\t  %-4s   %s" % [time, Filesize.from(size).pretty, read, name]
+  end
 end
